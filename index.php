03c2ee3f5216b6e7b33720dc48dd7f9dc8f9cd8f
Nicolas Devenet <github@devenet.info>
2013-11-27 20:20:34 +0100

    Add view for day and meta informations

--- index.php
+++ index.php
@@ -13,42 +13,56 @@ if (file_exists('settings.json')) {
 
     define('TITLE', $settings->title);
     define('YEAR', $settings->year);
-    if (empty($settings->keypass)) { die('<div><strong>Oups!</strong> Invalid syntax in settings file: passkey not found.</div>'); }
-    define('KEYPASS', $settings->keypass);
-    
 }
 else { die('<div><strong>Oups!</strong> Settings file not found.</div>'); }
 
 // other constants to be used
-define('VERSION', '0.1.0');
-define('PHPPREFIX','<?php header("Location: ./"); exit(); /* ');
-define('PHPSUFFIX',' */ ?>');
-define('TOKENS_FILE', 'tokens.php');
+define('VERSION', '0.2.0');
 define('URL_DAY', 'day');
-define('URL_SEPARATOR', '/');
+define('CALENDAR_FILE', './photos/calendar.json');
 
 // is the directory writable ?
 if (!is_writable(realpath(dirname(__FILE__)))) die('<div><strong>Oups!</strong> Application does not have the right to write in its own directory <code>'.realpath(dirname(__FILE__)).'</code>.</div>');
 // are photos deny from web access ?
 if (!is_file('photos/.htaccess')) { file_put_contents('photos/.htaccess', 'Deny from all'); }
 if (!is_file('photos/.htaccess')) die('<div><strong>Oups!</strong> Application does not have the right to write in its own directory <code>'.realpath(dirname(__FILE__)).'</code>.</div>');
-// have we the url token file loaded and up to date ?
-if (!is_file(TOKENS_FILE)) { Advent::generateTokens(); }
-else { Advent::loadTokens(); }
 
 /*
- *	Advent class
+ *	Core classes
  */
+abstract class Image {
+	function get($day) {
+		// check if we can display the request photo
+		if (Advent::acceptDay($day) && Advent::isActiveDay($day)) {
+			$day = $day+1;
+			$extension = '.jpg';
+			// if .jpg does not exist, load .jpeg photo
+			if (! self::exists($day.$extension)) {
+				$extension = '.jpeg';
+				// in case of .jpg or .jpeg file is not found
+				if (! self::exists($day.$extension)) { die('<div><strong>Oups!</strong> Unable to load photo.</div>'); }
+			}
+			$photo = file_get_contents('./photos/'.$day.$extension);
+			header('Content-type: image/jpeg');
+			exit($photo);
+		}
+		header('Location: ./');
+		exit();
+	}
+	
+	private function exists($file) {
+		return file_exists('./photos/'.$file);
+	}
+}
+ 
 abstract class Advent {
 	
 	const DAYS = 24;
 	const BEFORE_ADVENT = -1;
 	const CURRENT_ADVENT = 0;
 	const AFTER_ADVENT = 1;
-	const BEGIN_DATE = 1101;
+	const BEGIN_DATE = 1201;
 	const END_DATE = 1224;
-	
-	private static $tokens;
 	
 	function state() {
 		$now = date('Ymd');
@@ -61,7 +75,11 @@ abstract class Advent {
 		return self::CURRENT_ADVENT;
 	}
 	
-	private function isActiveDay($day) {
+	function acceptDay($day) {
+		return $day >= 0 && $day <= self::DAYS;
+	}
+	
+	function isActiveDay($day) {
 		$state = self::state();
 		return ($state == self::CURRENT_ADVENT && $day < date('d')) || $state == self::AFTER_ADVENT;
 	}
@@ -75,11 +93,11 @@ abstract class Advent {
 		return $result;
 	}
 	
-	function daysList() {
-		$result = '<div class="container container-days">';
+	function getDays() {
+		$result = '<div class="container days">';
 		for ($i=0; $i<self::DAYS; $i++) {
 			$active = self::isActiveDay($i);
-			if ($active) { $result .= '<a href="?'. URL_DAY .'='. ($i+1).URL_SEPARATOR. self::$tokens[$i] .'" title="Day '. ($i+1) .'"'; }
+			if ($active) { $result .= '<a href="?'. URL_DAY .'='. ($i+1) .'" title="Day '. ($i+1) .'"'; }
 			else { $result .= '<div'; }
 			$result .= ' class="day-row '. self::getDayColorClass($i, $active) .'"><span>'. ($i+1) .'</span>';
 			if ($active) { $result .= '</a>'; }
@@ -88,28 +106,93 @@ abstract class Advent {
 		return $result.'</div>';
 	}
 	
-	function generateTokens() {
-		$tokens = array();
-		$tokens['verification'] = sha1(KEYPASS);
-		for ($i=0; $i<self::DAYS; $i++) { $tokens[$i] = md5(KEYPASS.$i.KEYPASS); }
-		file_put_contents(TOKENS_FILE, PHPPREFIX.serialize($tokens).PHPSUFFIX);
-		if (!is_file(TOKENS_FILE)) { die('<div><strong>Oups!</strong> Application does not have the right to write in its own directory <code>'.realpath(dirname(__FILE__)).'</code>.</div>'); }
-		self::$tokens = $tokens;
+	function getDay($day) {
+		$result = '<div class="container day">';
+		
+		// check if we have info to display
+		if (file_exists(CALENDAR_FILE)) {
+			$file = json_decode(file_get_contents(CALENDAR_FILE));
+			if (!empty($file->{$day+1})) {
+				if (!empty($file->{$day+1}->title)) { $title = htmlspecialchars($file->{$day+1}->title); }
+				if (!empty($file->{$day+1}->legend)) { $legend = htmlspecialchars($file->{$day+1}->legend); }
+				if (!empty($file->{$day+1}->text)) { $text = $file->{$day+1}->text; }
+			}
+		}
+		
+		// set the day number block 
+		$result .= '<a href="./?'. URL_DAY.'='. ($day+1) .'" class="day-row '. self::getDayColorClass($day, TRUE) .'"><span>'. ($day+1) .'</span></a>';
+		// set the title
+		$result .= '<h1><span>';
+		if (!empty($title))	{ $result .= $title; }
+		else { $result .= 'Day '.($day+1); }
+		$result .= '</span></h1>';
+		// clearfix
+		$result .= '<div class="clearfix"></div>';
+		
+		// display image
+		$result .= '<div class="text-center"><img src="./?photo='. ($day+1) .'" class="img-responsive img-thumbnail" alt="Day '. ($day+1) .'" />';
+		// do we have a legend?
+		if (!empty($legend)) { $result .= '<p class="legend">&mdash; '.$legend.'</p>'; }
+		$result .= '</div>';
+		// clearfix
+		$result .= '<div class="clearfix"></div>';
+		
+		// do we have a text?
+		if (!empty($text)) { $result .= '<div class="text panel panel-default"><div class="panel-body">'.$text.'</div></div>'; }
+		
+		// we do not forget the pagination
+		$result .= '<ul class="pager"><li class="previous';
+		if (self::isActiveDay($day-1) && ($day-1)>=0) { $result .= '"><a href="?'. URL_DAY .'='. $day .'" title="yesterday" class="tip" data-placement="right">'; }
+		else { $result .= ' disabled"><a>'; }
+		$result .= '<i class="glyphicon glyphicon-hand-left"></i></a></li><li class="next';
+		if (self::isActiveDay($day+1)) { $result .= '"><a href="?'. URL_DAY .'='. ($day+2) .'" title="tomorrow" class="tip" data-placement="left">'; }
+		else { $result .= ' disabled"><a>'; }
+		$result .= '<i class="glyphicon glyphicon-hand-right"></i></a></li></ul>';
+		
+		return $result.'</div>';
 	}
 	
-	function loadTokens() {
-		self::$tokens = unserialize(substr(file_get_contents(TOKENS_FILE),strlen(PHPPREFIX),-strlen(PHPSUFFIX)));
-		if ($tokens['verification'] != sha1(KEYPASS)) { self::generateTokens(); }
-	}
-	
-	function isActiveLink($link) {
-		$link = explode(URL_SEPARATOR, $link);
-		$link_day = $link[0]-1;
-		return ($link_day >= 0 && $link_day <= self::DAYS) && self::isActiveDay($link_day);
+	function bePatient($day) {
+		return '<div class="container error"><div class="panel panel-info"><div class="panel-heading"><h3 class="panel-title">Christmas is coming soon!</h3></div><div class="panel-body">But before, <strong>be patient</strong>, day '. ($day+1) .' is only in few days. <a href="./" class="illustration text-center tip" title="home"><i class="glyphicon glyphicon-home"></i></a></div></div></div>';
 	}
 		
 }
 
+/*
+ * Load template
+ */
+ 
+if (isset($_GET['photo'])) { Image::get($_GET['photo']-1); }
+
+$template = NULL;
+
+// nothing asked, display homepage
+if (empty($_GET)) {
+	$template = Advent::getDays();		
+}
+// want to display a day
+if (isset($_GET['day'])) {
+	$day = $_GET['day'] - 1;
+	if (! Advent::acceptDay($day)) { header('Location: ./'); exit(); }
+	if (Advent::isActiveDay($day)) {
+		$template = Advent::getDay($day);
+	}
+	else { $template = Advent::bePatient($day); }
+}
+
+// want to display about page
+if (isset($_GET['about'])) {
+	// if ugly URL
+	if (!empty($_GET['about'])) { header('Location: ./?about'); exit(); }
+	$template = file_get_contents('./assets/about.html');
+}
+
+// default template is 404
+if (empty($template)) {
+	$template = '<div class="container error"><div class="panel panel-danger"><div class="panel-heading"><h3 class="panel-title">404 Not Found</h3></div><div class="panel-body">The requested URL was not found on this server. <a href="./" class="illustration illustration-danger text-center tip" title="home"><i class="glyphicon glyphicon-home"></i></a></div></div></div>';
+	header('HTTP/1.1 404 Not Found', true, 404);
+}
+ 
 
 ?><!doctype html>
 <html lang="fr">
@@ -136,48 +219,32 @@ abstract class Advent {
 		<div class="container">
 		<div class="navbar-header">
 		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-		  <span class="sr-only">Toggle navigation</span>
+		  <span class="sr-only">navigation</span>
 		  <span class="icon-bar"></span>
 		  <span class="icon-bar"></span>
 		  <span class="icon-bar"></span>
 		</button>
-		<a class="navbar-brand" href="./"><?php echo TITLE; ?></a>
+		<a class="navbar-brand tip" href="./" title="home" data-placement="right"><?php echo TITLE; ?></a>
 		</div>
 		
-		<!-- Collect the nav links, forms, and other content for toggling -->
-		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		<div class="collapse navbar-collapse">
 		<ul class="nav navbar-nav navbar-right">
-		  <li><a href="./"><i class="glyphicon glyphicon-tree-conifer"></i> Advent Calendar</a></li>
+		  <li><a href="./?about" class="tip" data-placement="left" title="about"><i class="glyphicon glyphicon-tree-conifer"></i> Advent Calendar</a></li>
 		</ul>
-		</div><!-- /.navbar-collapse -->
+		</div>
 		</div>
 		</nav>
-
 		
+		<div class="background">
 		<?php
-		
-		// nothing asked, display homepage
-		if (empty($_GET)) {
-			echo Advent::daysList();			
-		}
-		// want to display a day
-		if (isset($_GET['day'])) {
-			if (Advent::isActiveLink(htmlspecialchars($_GET['day']))) {
-				
-			}
-			else {
-				
-			}
-		}
-			
+			echo $template;
 		?>
-
+		</div>
 		
-		<hr />
 		<footer>
+		<hr />
 		<div class="container">
-			<p class="pull-right"><a href="#" id="goHomeYouAreDrunk" class="tip" data-placement="top" title="upstairs"><i class="glyphicon glyphicon-tree-conifer"></i></a></p>
-		
+			<p class="pull-right"><a href="#" id="goHomeYouAreDrunk" class="tip" data-placement="left" title="upstairs"><i class="glyphicon glyphicon-tree-conifer"></i></a></p>
 			Advent Calendar &middot; Version <?php echo VERSION; ?>
 			<br />Developped with love by <a href="http://devenet.info" rel="external">Nico</a>.
 		</div>
@@ -185,6 +252,7 @@ abstract class Advent {
 		
     	<script src="https://code.jquery.com/jquery.js"></script>
     	<script src="assets/bootstrap.min.js"></script>
+    	<script src="assets/snowfall.min.js"></script>
     	<script src="assets/adventcalendar.js"></script>
 	</body>
 </html>
